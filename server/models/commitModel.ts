804e008cd9b2861ef99eb798f7e4767d5f4c824a
Alexander Maliarevskii <luexe@yandex.ru>
2024-06-19 00:16:13 +0300

    add controller, model and service

---
+++ server/models/commitModel.ts
@@ -0,0 +1,15 @@
+export interface Commit {
+  sha: string;
+  commit: {
+    author: {
+      name: string;
+      email: string;
+      date: string;
+    };
+    message: string;
+  };
+  author: {
+    login: string;
+    avatar_url: string;
+  };
+}
