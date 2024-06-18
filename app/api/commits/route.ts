95d0c008cc4431baf10b238e0c7ac28b1a3b4c7b
Alexander Maliarevskii <luexe@yandex.ru>
2024-06-18 19:17:38 +0300

    update route commits

--- app/api/commits/route.ts
+++ app/api/commits/route.ts
@@ -1,25 +1,34 @@
 import { NextResponse } from "next/server";
 import { Octokit } from "@octokit/rest";
 
-console.log("GITHUB_TOKEN:", process.env.GITHUB_TOKEN);
-
 const octokit = new Octokit({
   auth: process.env.GITHUB_TOKEN,
 });
 
 export async function GET() {
+  console.log("Fetching commits...");
+
   try {
     const { data } = await octokit.repos.listCommits({
       owner: "iRiddle",
       repo: "my-git-app",
       per_page: 10,
+      headers: {
+        "Cache-Control": "no-store",
+        Pragma: "no-cache",
+      },
     });
 
-    console.log(data);
+    console.log("Fetched commits:", data);
 
-    return NextResponse.json(data);
+    return NextResponse.json(data, {
+      headers: {
+        "Cache-Control": "no-store, max-age=0",
+        Pragma: "no-cache",
+      },
+    });
   } catch (error) {
-    console.error(error);
+    console.error("Error fetching commits:", error);
     return NextResponse.error();
   }
 }
