f85a3960589b9e38917c22786c16422ea62c68d3
Alexander Maliarevskii <luexe@yandex.ru>
2024-06-18 19:19:22 +0300

    update commit code

--- app/shared/Commit/Commit.tsx
+++ app/shared/Commit/Commit.tsx
@@ -1,12 +1,37 @@
-import { FC } from "react";
-
+import React from "react";
 import { ICommit } from "@/app/interfaces/ICommit";
 
-interface ICommitProps {
+interface CommitProps {
   commit: ICommit;
 }
 
-export const Commit: FC<ICommitProps> = ({ commit }) => {
-  console.log(commit);
-  return <div>Commit</div>;
+export const Commit: React.FC<CommitProps> = ({ commit }) => {
+  const commitDate = new Date(commit.commit.author.date);
+
+  const localeDate = commitDate.toLocaleString("ru-RU", {
+    day: "numeric",
+    month: "long",
+  });
+
+  const localeTime = commitDate.toLocaleString("ru-RU", {
+    hour: "2-digit",
+    minute: "2-digit",
+    hour12: false,
+  });
+
+  return (
+    <div className="border border-gray-300 rounded-lg p-6 mb-5 transition duration-300 hover:border-blue-500 hover:text-blue-500 cursor-pointer">
+      <span className="font-bold text-lg">{commit.commit.message}</span>
+      <div className="flex justify-between items-center">
+        <span className="text-sm text-gray-600">
+          by {commit.commit.author.name}
+        </span>
+        <span className="text-sm text-gray-400">
+          {`${localeDate} в ${localeTime}`}
+        </span>
+      </div>
+    </div>
+  );
 };
+
+export default Commit;
